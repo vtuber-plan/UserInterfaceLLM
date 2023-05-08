fe166990b22b77b28dbf2f91e63f69ea36ccf912
FrostMiKu <i@frostmiku.com>
2023-05-09 00:53:40 +0800

    async

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 from enum import Enum
+import httpx
 import requests
 import json
 from typing import Generator, List
 
 BASE_URL = 'http://127.0.0.1:8000'
 
-def get_resp_stream(ctx: List[dict], model:str, temperature:float=0.7, top_p:float=0.9) -> Generator:
+async def get_resp_stream(ctx: List[dict], model:str, temperature:float=0.7, top_p:float=0.9) -> Generator:
     packet = {
         "model": model,
         "messages": ctx,
@@ -14,15 +15,33 @@ def get_resp_stream(ctx: List[dict], model:str, temperature:float=0.7, top_p:flo
         "top_p": top_p,
         "stream": True,
     }
-    res = requests.post(
-        url=f'{BASE_URL}/v1/chat/completions', json=packet, stream=True)
-    if res.status_code != 200:
-        raise Exception(res)
-    for chunk in res.iter_lines():
-        data = chunk.decode()[6:]
-        if len(data) == 0 or data[-1] != '}':
-            continue
-        yield json.loads(data)
+    # res = requests.post(
+    #     url=f'{BASE_URL}/v1/chat/completions', json=packet, stream=True)
+    async with httpx.AsyncClient() as client:
+            async with client.stream(
+                "POST",
+                f"{BASE_URL}/v1/chat/completions",
+                json=packet,
+                timeout=20,
+            ) as response:
+                async for chunk in response.aiter_text():
+                    if not chunk:
+                        continue
+                    for line in chunk.split("\n\n"):
+                        if not line:
+                            continue
+                        if line.startswith("data: "):
+                            line = line.removeprefix("data: ")
+                        if line.strip() == "[DONE]":
+                            break
+                        yield json.loads(line)
+    # if res.status_code != 200:
+    #     raise Exception(res)
+    # for chunk in res.iter_lines():
+    #     data = chunk.decode()[6:]
+    #     if len(data) == 0 or data[-1] != '}':
+    #         continue
+    #     yield json.loads(data)
 
 
 def get_resp(ctx: List[dict], model:str, temperature:float=0.7, top_p:float=0.9) -> str:
@@ -53,9 +72,10 @@ def gen_openai_message(content: str, role: Role) -> dict:
         "content": content
     }
 
-def pretty_print_json(json_string:str):
-    json_obj = json.loads(json_string)
-    print(json.dumps(json_obj, indent=4, sort_keys=True))
+def pretty_print_json(data:str|dict):
+    if isinstance(data, str):
+        data = json.loads(data)
+    print(json.dumps(data, indent=4, sort_keys=True))
 
 if __name__ == '__main__':
     mock_ctx = [{"role": "system", "content": "你是一个名为Ice的人工智能助手"},{"role": "user", "content": "你好啊"}]
--- webui_auth.py
+++ webui_auth.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from typing import Generator
 import streamlit as st
 import streamlit_authenticator as stauth
-from utils import get_resp_stream, gen_openai_message, Role
+from utils import get_resp_stream, gen_openai_message, Role, pretty_print_json
+import asyncio
 
 MODEL_NAME = 'ningyu'
 BOT_NAME = "凝语"
@@ -56,8 +57,6 @@ if 'authentication_status' not in st.session_state:
 hashed_passwords = stauth.Hasher([USERS[name]['password'] for name in USERS]).generate()
 for i, name in enumerate(USERS):
     USERS[name]['password'] = hashed_passwords[i]
-authenticator = stauth.Authenticate({'usernames':USERS},cookie_name=MODEL_NAME,key=SIGNATURE_KEY, cookie_expiry_days=7)
-
 
 def format_ctx(history=None)->str:
     text = ""
@@ -72,7 +71,7 @@ def format_ctx(history=None)->str:
     return text
 
 
-def predict(ctx) -> Generator:
+async def predict(ctx) -> Generator:
     if ctx is None:
         ctx = []
 
@@ -85,58 +84,65 @@ def predict(ctx) -> Generator:
         prefix_list.append(gen_openai_message(
             f"请注意，本次对话的主题是{st.session_state.title}。", Role.User))
 
-    for data in get_resp_stream(prefix_list+ctx, MODEL_NAME, 0.7):
+    async for data in get_resp_stream(prefix_list+ctx, MODEL_NAME, 0.7):
         if data['choices'][0]['finish_reason'] == "stop":
             break
+        if 'content' not in data['choices'][0]['delta']:
+            continue
         yield data['choices'][0]['delta']['content']
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+async def main():
+    authenticator = stauth.Authenticate({'usernames':USERS},cookie_name=MODEL_NAME,key=SIGNATURE_KEY, cookie_expiry_days=7)
+    name, authentication_status, username = authenticator.login('Login', 'main')
 
-if st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"]:
-    # authenticator.logout('Logout', 'main')
-    title_dom = st.title(st.session_state.title)
-    ctx_dom = st.empty()
-    md_dom = st.markdown(
-        f"> 当前最大上下文长度：{MAX_CONTEXT}\n\n:face_with_cowboy_hat::\n你好 👋")
+    if st.session_state["authentication_status"] is None:
+        st.warning('Please enter your username and password')
+    elif st.session_state["authentication_status"] is False:
+        st.error('Username/password is incorrect')
+    elif st.session_state["authentication_status"]:
+        # authenticator.logout('Logout', 'main')
+        title_dom = st.title(st.session_state.title)
+        ctx_dom = st.empty()
+        md_dom = st.markdown(
+            f"> 当前最大上下文长度：{MAX_CONTEXT}\n\n:face_with_cowboy_hat::\n你好 👋")
 
-    with st.form("form", True):
-        prompt_text = st.text_area(label=":thinking_face: 聊点什么？",
-                                height=100,
-                                max_chars=2048,
-                                placeholder="支持使用 Markdown 格式书写")
-        col1, col2 = st.columns([1, 1])
-        with col1:
-            btn_send = st.form_submit_button(
-                "发送", use_container_width=True, type="primary")
-        with col2:
-            btn_clear = st.form_submit_button("开始新的会话", use_container_width=True)
+        with st.form("form", True):
+            prompt_text = st.text_area(label=":thinking_face: 聊点什么？",
+                                    height=100,
+                                    max_chars=2048,
+                                    placeholder="支持使用 Markdown 格式书写")
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                btn_send = st.form_submit_button(
+                    "发送", use_container_width=True, type="primary")
+            with col2:
+                btn_clear = st.form_submit_button("开始新的会话", use_container_width=True)
 
-        if btn_send and prompt_text != "":
-            st.session_state.ctx.append(gen_openai_message(prompt_text, Role.User))
-            md_dom.markdown(":face_with_cowboy_hat::\n请稍后...")
-            ctx_dom.markdown(format_ctx(st.session_state.ctx))
-            text = ''
-            for delta in predict(st.session_state.ctx):
-                text += delta
-                md_dom.markdown(":face_with_cowboy_hat::\n"+text)
-            st.session_state.ctx.append(gen_openai_message(text, Role.Bot))
+            if btn_send and prompt_text != "":
+                st.session_state.ctx.append(gen_openai_message(prompt_text, Role.User))
+                md_dom.markdown(":face_with_cowboy_hat::\n请稍后...")
+                ctx_dom.markdown(format_ctx(st.session_state.ctx))
+                text = ''
+                async for delta in predict(st.session_state.ctx):
+                    text += delta
+                    md_dom.markdown(":face_with_cowboy_hat::\n"+text)
+                st.session_state.ctx.append(gen_openai_message(text, Role.Bot))
 
-            if st.session_state.first_run:
-                st.session_state.first_run = False
-                text = '# '
-                for delta in predict(st.session_state.ctx+[gen_openai_message("请为以上对话生成一个合适的标题，不要超过10个字，请直接输出标题并用书名号“《》”包括", Role.User)]):
-                    text += delta.replace("\n", "").replace("《", "").replace("》", "")
-                    title_dom.title(text)
-                st.session_state.title = text
-                st.balloons()
+                if st.session_state.first_run:
+                    st.session_state.first_run = False
+                    text = '# '
+                    async for delta in predict(st.session_state.ctx+[gen_openai_message("请为以上对话生成一个合适的标题，不要超过10个字，请直接输出标题并用书名号“《》”包括", Role.User)]):
+                        text += delta.replace("\n", "").replace("《", "").replace("》", "")
+                        title_dom.title(text)
+                    st.session_state.title = text
+                    st.balloons()
 
-        if btn_clear:
-            ctx_dom.empty()
-            title_dom.title(TITLE)
-            st.session_state.ctx = []
-            st.session_state.first_run = True
-            st.session_state.title = TITLE
+            if btn_clear:
+                ctx_dom.empty()
+                title_dom.title(TITLE)
+                st.session_state.ctx = []
+                st.session_state.first_run = True
+                st.session_state.title = TITLE
+
+if __name__ == "__main__":
+    asyncio.run(main())
